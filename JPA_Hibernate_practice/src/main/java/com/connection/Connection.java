a03dcbabefd4ba0eb502ec13a1fb796b7b834034
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-09-22 23:34:48 +0530

    Student class phued

--- JPA_Hibernate_practice/src/main/java/com/connection/Connection.java
+++ JPA_Hibernate_practice/src/main/java/com/connection/Connection.java
@@ -1,5 +1,30 @@
 package com.connection;
 
-public class Connection {
+import com.models.Students;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Connection {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("studentUnit1");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		Students s1 = new Students();
+		
+		s1.setName("Prakesh");
+		s1.setMarks(780);
+		
+		em.getTransaction().begin();
+		em.persist(s1);
+		em.getTransaction().commit();
+		
+		em.close();
+		
+		System.out.println("inserting of Student 1 is done...");
+	}
 }
